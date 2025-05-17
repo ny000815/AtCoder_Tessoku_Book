12d41aeb00d171fa799c00d5d72eea64f6dc01cd
Yoshi <66006097+ny000815@users.noreply.github.com>
2025-05-17 13:09:26 +0900

    Create A14.cpp

---
+++ A14.cpp
@@ -0,0 +1,13 @@
+#include <iosteream>
+using namespace std;
+
+int N, K;
+int A[1009], B[1009], C[1009], D[1009], P[100009], Q[100009];
+
+int main(){
+    cin >> N >> K;
+    for (char X = A; i<= D; X++){
+        for (int i = 1; i <= N; i++) cin >> X[i];
+     }
+    return 0
+}
